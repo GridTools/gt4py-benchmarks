f11567ca7dcf2235797a1c091034f4895343ce57
Felix Thaler <thaler@cscs.ch>
2020-11-24 10:32:51 +0100

    Fix execution with gtcuda

--- src/gt4py_benchmarks/numerics/stencil_backends/gt4py_backend.py
+++ src/gt4py_benchmarks/numerics/stencil_backends/gt4py_backend.py
@@ -62,6 +62,8 @@ class GT4PyStencilBackend(base.StencilBackend):
         self._scalar = np.dtype(self.dtype).type
 
     def _modified_gt4py_backend(self, resolution):
+        if 'dace' not in self.gt4py_backend:
+            return self.gt4py_backend
         backend = gt4py.backend.base.from_name(self.gt4py_backend)
         new_name = self.gt4py_backend + "_" + "x".join(str(r) for r in resolution)
         assert backend.storage_info["layout_map"]([True] * 3) == (2, 1, 0)
